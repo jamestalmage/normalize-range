16df0966710268215c133858d376c79ebb04bb65
James Talmage <james@talmage.io>
2015-09-11 20:34:39 -0400

    add limit, validate, and test

--- index.js
+++ index.js
@@ -1,14 +1,46 @@
 'use strict';
-normalizeRange.curry = curry;
-module.exports = normalizeRange;
+module.exports = {
+  wrap: wrapRange,
+  limit: limitRange,
+  validate: validateRange,
+  test: testRange,
+  curry: curry
+};
 
-function normalizeRange(min, max, value) {
+function wrapRange(min, max, value) {
   var maxLessMin = max - min;
   return ((value - min) % maxLessMin + maxLessMin) % maxLessMin + min;
 }
 
-function curry(min, max) {
-  return function(value) {
-    return normalizeRange(min, max, value);
+function limitRange(min, max, value) {
+  return Math.max(min, Math.min(max, value));
+}
+
+function validateRange(min, max, value, minExclusive, maxExclusive) {
+  if (!testRange(min, max, value, minExclusive, maxExclusive)) {
+    throw new Error(value + ' is outside of range [' + min + ',' + max + ')');
+  }
+  return value;
+}
+
+function testRange(min, max, value, minExclusive, maxExclusive) {
+  return !(
+       value < min ||
+       value > max ||
+       (maxExclusive && (value === max)) ||
+       (minExclusive && (value === min))
+  );
+}
+
+function curry(min, max, minExclusive, maxExclusive) {
+  return {
+    wrap: wrapRange.bind(null, min, max),
+    limit: limitRange.bind(null, min, max),
+    validate: function(value) {
+      return validateRange(min, max, value, minExclusive, maxExclusive);
+    },
+    test: function(value) {
+      return testRange(min, max, value, minExclusive, maxExclusive);
+    }
   };
 }
--- test/test.js
+++ test/test.js
@@ -1,56 +1,74 @@
 'use strict';
 var assert = require('assert');
-var normalizeRange = require('../');
+var ranges = require('../');
 
-describe('normalize-range', function() {
-  var π = Math.PI;
+var π = Math.PI;
 
-  function close(a, b) {
-    return Math.abs(a - b) < 0.00000001;
+function close(a, b) {
+  return Math.abs(a - b) < 0.00000001;
+}
+
+function namePi(val) {
+  if (typeof val !== 'number') {
+    return val;
   }
-
-  function name (val) {
-    for (var numerator = 1; numerator <= 10; numerator++) {
-      for (var denom = 1; denom <= 10; denom++) {
-        if (close(Math.abs(val), (numerator * π / denom))) {
-          var s = val < 0 ? '-' : '';
-          if (numerator !== 1) {
-            s += numerator;
-          }
-          s += 'π';
-          if (denom !== 1) {
-            s += '/' + denom;
-          }
-          return s;
+  for (var numerator = 1; numerator <= 10; numerator++) {
+    for (var denom = 1; denom <= 10; denom++) {
+      if (close(Math.abs(val), (numerator * π / denom))) {
+        var s = val < 0 ? '-' : '';
+        if (numerator !== 1) {
+          s += numerator;
         }
+        s += 'π';
+        if (denom !== 1) {
+          s += '/' + denom;
+        }
+        return s;
       }
     }
-    return '' + val;
+  }
+  return '' + val;
+}
+
+function names(type, min, max, value, expected, minExcl, maxExcl) {
+  var lb = minExcl ? '(' : '[';
+  var rb = maxExcl ? ')' : ']';
+  var rangeName = type +  ': ' + lb + namePi(min) + ',' + namePi(max) + rb;
+  var testName = rangeName + ' ' + namePi(value) + ' === ' + namePi(expected);
+
+  return {
+    range: rangeName,
+    test: testName
+  };
+}
+
+function _test(type, min, max, value, expected, minExcl, maxExcl) {
+  var n = names(type, min, max, value, expected, minExcl, maxExcl);
+  it(n.test, function() {
+    var result = ranges[type](min, max, value, minExcl, maxExcl);
+    if (!close(result, expected)) {
+      assert.strictEqual(
+        result,
+        expected,
+        n.range
+      );
+    }
+  });
+}
+
+describe('wrap ', function() {
+  function test(min, max, value, expected) {
+    return _test('wrap', min, max, value, expected, false, true);
   }
 
-  function test(min, max, value, expected, testName) {
-    var rangeName = '[' + name(min) + '-' + name(max) + ')';
-    testName = testName || (rangeName + ' ' + name(value) + ' === ' + name(expected));
-    it(testName, function() {
-      var result = normalizeRange(min, max, value);
-      if (!close(result, expected)) {
-        assert.strictEqual(
-          normalizeRange(min, max, value),
-          expected,
-          rangeName
-        );
-      }
-    });
-  }
-
-  var angleCurry = normalizeRange.curry(0, 360);
+  var angleWrap = ranges.curry(0, 360).wrap;
   function angle(value, expected) {
     test(0, 360, value, expected);
 
-    var message = 'angleCurry(' + value + ') === ' + expected;
+    var message = 'angleWrap(' + value + ') === ' + expected;
     it(message, function() {
       assert.strictEqual(
-        angleCurry(value),
+        angleWrap(value),
         expected,
         message
       );
@@ -83,4 +101,102 @@ describe('normalize-range', function() {
   test(-π, π, 2 * π, 0);
   test(-π, π, 2 * π / 3, 2 * π / 3);
   test(-π, π, 4 * π / 3, -2 * π / 3);
+});
+
+describe('limit', function() {
+  var test = _test.bind(null, 'limit');
+
+  test(0, 10, -1, 0);
+  test(0, 10, 0, 0);
+  test(0, 10, 5, 5);
+  test(0, 10, 10, 10);
+  test(0, 10, 11, 10);
+});
+
+describe('test', function() {
+  function isOk(min, max, value, minExcl, maxExcl, ok) {
+    var n = names('test', min, max, value, ok ? 'ok' : 'notOk', minExcl, maxExcl);
+
+    it(n.test, function() {
+      assert.strictEqual(
+        !!ok,
+        ranges.test(min, max, value, minExcl, maxExcl),
+        n.test
+      );
+    });
+  }
+
+  function ok(min, max, value, minExcl, maxExcl) {
+    isOk(min, max, value, minExcl, maxExcl, true);
+  }
+
+  function notOk(min, max, value, minExcl, maxExcl) {
+    isOk(min, max, value, minExcl, maxExcl, false);
+  }
+
+  notOk(0, 10, -1);
+  notOk(0, 10, 0, true);
+  ok(0, 10, 0);
+  ok(0, 10, 9);
+  ok(0, 10, 10);
+  notOk(0, 10, 10, false, true);
+  notOk(0, 10, 11);
+
+  it('curried', function() {
+    var c = ranges.curry(0, 10, false, true).test;
+    assert.strictEqual(c(0), true);
+    assert.strictEqual(c(10), false);
+    assert.strictEqual(c(8), true);
+    assert.strictEqual(c(11), false);
+  });
+});
+
+describe('validate', function() {
+
+  function isOk(min, max, value, minExcl, maxExcl, ok) {
+    var n = names('test', min, max, value, ok ? 'ok' : 'notOk', minExcl, maxExcl);
+
+    it(n.test, function() {
+      if (ok) {
+        assert.strictEqual(
+          ranges.validate(min, max, value, minExcl, maxExcl),
+          value,
+          n.test
+        );
+      } else {
+        assert.throws(function() {
+          ranges.validate(min, max, value, minExcl, maxExcl);
+        }, null, n.test);
+      }
+
+    });
+  }
+
+  function ok(min, max, value, minExcl, maxExcl) {
+    isOk(min, max, value, minExcl, maxExcl, true);
+  }
+
+  function notOk(min, max, value, minExcl, maxExcl) {
+    isOk(min, max, value, minExcl, maxExcl, false);
+  }
+
+  notOk(0, 10, -1);
+  notOk(0, 10, 0, true);
+  ok(0, 10, 0);
+  ok(0, 10, 9);
+  ok(0, 10, 10);
+  notOk(0, 10, 10, false, true);
+  notOk(0, 10, 11);
+
+  it('curried', function() {
+    var c = ranges.curry(0, 10, true, false).validate;
+    assert.throws(function() {
+      c(0);
+    });
+    assert.strictEqual(c(10), 10);
+    assert.strictEqual(c(8), 8);
+    assert.throws(function() {
+      c(11);
+    });
+  });
 });
